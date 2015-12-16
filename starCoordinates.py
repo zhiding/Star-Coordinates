7cc1bba7b77c4001ff460a448bd697b68321e3f2
zhiding <zhiding.sh@gmail.com>
2015-12-17 01:29:33 +0800

    result visualization

--- starCoordinates.py
+++ starCoordinates.py
@@ -1,11 +1,12 @@
-import csv, re
+import csv, math, re
 import numpy as np
 import matplotlib.pyplot as plot
+from matplotlib.patches import Circle
 
 ########################################################################################
 ## Attributes: MPG;Cylinders;Displacement;Horsepower;Weight;Acceleration;Model;Origin ##
 ########################################################################################
-def csv2mat(filepath, primary_key):
+def csv2mat(filepath, removed_keys):
     data_mat = []
     with open(filepath, 'rb') as csvfile:
         items = csv.reader(csvfile)
@@ -78,6 +79,73 @@ def nD2cc(num_of_attrs):
     np_cc = np.matrix(cc_cos[:-1]+';'+cc_sin[:-1])
     return np_cc
 
+##################################################################
+## Usage: Display the scatter figure to show data in 2D from nD ##
+##################################################################
+def figure(np_coord, np_data):
+    ## scatters
+    np_osc = np.dot(np_coord, np_data.T)
+    coord_x = np.array(np_coord[0,:]).tolist()
+    coord_y = np.array(np_coord[1,:]).tolist()
+    osc_x = np.array(np_osc[0,:]).tolist()
+    osc_y = np.array(np_osc[1,:]).tolist()
+    fig = plot.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(osc_x, osc_y, color='#5CACEE')
+    r_max = 0.0
+    dims = np_coord.size / 2
+    for i in range(dims):
+        ## lines 
+        ax.plot([np_coord[0,i],0], [np_coord[1,i],0], linewidth=1.5, color='#FF6A6A')
+        ax.plot(np_coord[0,i], np_coord[1,i], 'o', color='#7FFFD4')
+        ## circles 
+        r = math.sqrt(np_coord[0,i]*np_coord[0,i]+np_coord[1,i]*np_coord[1,i])
+        if r > r_max:
+            r_max = r
+        circle = Circle((0.0, 0.0), radius=r, fill=False, color='#B5B5B5')
+        ax.add_patch(circle)
+    ## axis
+    ax.axis('equal')
+    ax.axis('scaled')
+    return 0
+
+
+##############################################
+## Usage: Make star coordinates orthgraphic ##
+##############################################
+def osc(np_ori, method='rc'):
+    num_of_attrs = np_ori.size / 2
+    if method == 'em':
+        np_delta = np.matrix([[0 for i in range(num_of_attrs)] for j in range(2)])
+        [cost_ori, xx_ori, yy_ori, xy_ori] = cost(np_ori)
+        step = 0.01
+        error = 0.0001
+        for i in range(num_of_attrs):
+            np_delta[0,i] = 4 * np_ori[0,i] * (xx_ori - 1) + 2 * xy_ori * np_ori[1,i]
+            np_delta[1,i] = 4 * np_ori[1,i] * (yy_ori - 1) + 2 * xy_ori * np_ori[0,i]
+        np_gen = np_ori - step * np_delta
+        [cost_gen, xx_gen, yy_gen, xy_gen] = cost(np_gen)
+        print [cost_ori, xx_ori, yy_ori, xy_ori]
+        print [cost_gen, xx_gen, yy_gen, xy_gen]
+        print np_ori
+        print np_gen
+    else:
+        pass
+    return np_gen
+
+def cost(np_sc):
+    num_of_attrs = np_sc.size / 2 
+    sum_of_xx = 0
+    sum_of_yy = 0
+    sum_of_xy = 0
+    for i in range(num_of_attrs):
+        sum_of_xx += np_sc[0,i] * np_sc[0,i]
+        sum_of_yy += np_sc[1,i] * np_sc[1,i]
+        sum_of_xy += np_sc[0,i] * np_sc[1,i]
+    cost = (sum_of_xx - 1) * (sum_of_xx - 1) + (sum_of_yy - 1) * (sum_of_yy - 1) + sum_of_xy * sum_of_xy
+    return [cost, sum_of_xx, sum_of_yy, sum_of_xy]
+
+
 if __name__ == '__main__':
     data_mat = csv2mat('./cars.csv', 'Car')
     #data_mat = csv2mat('./test.csv', 'Test')
@@ -85,17 +153,9 @@ if __name__ == '__main__':
     np_mat = np.matrix(data_mat_reg)
     num_of_attrs = len(data_mat[0])
     np_cc = nD2cc(num_of_attrs)
-    sc_mat = np.dot(np_cc, np_mat.T)
-    sc_mat_len = sc_mat.size/sc_mat.ndim
-    sc_array = np.array(sc_mat).reshape(-1,).tolist()
-    sc_dx = sc_array[:sc_mat_len]
-    sc_dy = sc_array[sc_mat_len:]
-    cc_array = np.array(np_cc).reshape(-1,).tolist()
-    cc_x = cc_array[:num_of_attrs]
-    cc_y = cc_array[num_of_attrs:]
-    figure = plot.figure(1)
-    plot.scatter(sc_dy ,sc_dx)
-    for i in range(num_of_attrs):
-        plot.plot([cc_y[i],0],[cc_x[i],0])
+    figure(np_cc, np_mat)
+    np_cc[:,num_of_attrs-1] *= 5
+    figure(np_cc, np_mat)
+    np_gen = osc(np_cc, 'em')
+    figure(np_gen, np_mat)
     plot.show()
-
